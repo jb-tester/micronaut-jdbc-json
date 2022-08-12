e2b600dba73d77e6bb1783690b34df7ae2793a5e
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-08-12 11:35:01 +0200

    using native query with JSON_EXTRACT

--- src/main/java/com/mytests/micronaut/JsonController.java
+++ src/main/java/com/mytests/micronaut/JsonController.java
@@ -6,6 +6,7 @@ import com.mytests.micronaut.model.mapped_entities.JsonSampleMappedEntity;
 import com.mytests.micronaut.model.mapped_entities.JsonSampleMappedEntityRepository;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import jakarta.inject.Inject;
 
 import java.util.List;
@@ -28,5 +29,8 @@ public class JsonController {
     public List<JsonSampleMappedEntity> getAllMappedEntities() {
         return (List<JsonSampleMappedEntity>) mappedEntityRepository.findAll();
     }
-
+    @Get("/mappingEntitiesByProp/{prop}")
+    public List<JsonSampleMappedEntity> getByProp(@PathVariable String prop) {
+        return mappedEntityRepository.findByProperties(prop);
+    }
 }
--- src/main/java/com/mytests/micronaut/model/mapped_entities/JsonSampleMappedEntityRepository.java
+++ src/main/java/com/mytests/micronaut/model/mapped_entities/JsonSampleMappedEntityRepository.java
@@ -1,8 +1,12 @@
 package com.mytests.micronaut.model.mapped_entities;
 
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
+
+import java.util.List;
 
 
 @JdbcRepository(dialect = Dialect.MYSQL)
@@ -10,5 +14,6 @@ public interface JsonSampleMappedEntityRepository
         extends CrudRepository<JsonSampleMappedEntity,Integer>
 {
 
-
+    @Query("select * from micronaut.json_sample  where JSON_EXTRACT(properties, '$.prop11') = :value")
+    List<JsonSampleMappedEntity> findByProperties(@Parameter("value") String value);
 }
